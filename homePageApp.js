1b40cd124f7668b30d790371add73634c8b805a0
David Von Lehman <david.vonlehman@gmail.com>
2013-11-23 23:36:09 -0800

    Refactoring to use requires

---
+++ homePageApp.js
@@ -0,0 +1,24 @@
+require([
+  'angular',
+  'controllers/homePageCtrl'
+], function(angular, homePageCtrl) {
+  'use strict';
+  var $html = angular.element(document.getElementsByTagName('html')[0]);
+
+  console.log("HomePageApp loading");
+
+  // Load the template for the home page
+  require(["text!views/homePage.html"], function(view) {
+    var body = angular.element(document.getElementsByTagName(('body'))[0]);
+
+    // Append the view to the body
+    var angularDocument = angular.element(view);
+    body.append(angularDocument);
+
+    var homePageApp = angular.module('homePageApp', []);
+    angular.module('homePageApp').controller('HomePageCtrl', homePageCtrl);
+    
+    angular.bootstrap(angularDocument, ['homePageApp']);
+    return homePageApp;
+  });
+});
---
+++ views/homePage.html.js
@@ -0,0 +1,5 @@
+
+// Javascript version of html template
+define([], function() {
+  return "<div ng-app='homePageApp' ng-controller='HomePageCtrl'><h1>This is a view from Angular</h1><h2>{{name}}</h2></div>";
+});
