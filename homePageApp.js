371fa2618fc549e9d3d9f582f3414fcd0137fe35
David Von Lehman <david.vonlehman@gmail.com>
2013-11-27 23:21:58 -0800

    Loading data from the DOM

--- homePageApp.js
+++ homePageApp.js
@@ -1,22 +1,46 @@
 require([
   'angular',
+  'jquery',
+  'lodash',
   'configSettings',
   'controllers/homePageCtrl'
-], function(angular, configSettings, homePageCtrl) {
+], function(angular, jquery, _, configSettings, homePageCtrl) {
   'use strict';
   var $html = angular.element(document.getElementsByTagName('html')[0]);
 
   console.log("HomePageApp loading");
 
   // Load the template for the home page
-  require(["text!views/homePage.html"], function(view) {
-    var body = angular.element(document.getElementsByTagName(('body'))[0]);
+  var assets = [
+    "text!views/homePage.html",
+    "css!//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css"
+  ];
+
+  require(assets, function(view) {
+    var body = jquery(document.getElementsByTagName('body')[0]);
+
+    // Load the artists from the DOM
+    var homePageData = {
+      hotArtists: _.map(body.find("ul[data-hot-artists] > li a"), function(el) {
+        el = $(el);
+        return {
+          id: el.data("id"),
+          name: el.text()
+        }
+      })
+    };
 
     // Append the view to the body
     var angularDocument = angular.element(view);
     body.append(angularDocument);
 
     var homePageApp = angular.module('homePageApp', []);
+
+    // Define a homePageData service to inject the homePageData into the controller
+    homePageApp.factory('homePageData', [function() {
+      return homePageData;
+    }]);
+
     angular.module('homePageApp').controller('HomePageCtrl', homePageCtrl);
     
     angular.bootstrap(angularDocument, ['homePageApp']);
