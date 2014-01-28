776d29b3b7ecc3cacf3d4fae9276f535146ea3bf
David Von Lehman <david.vonlehman@gmail.com>
2014-01-28 08:56:52 -0800

    Artist detail page

--- controllers/artistDetailCtrl.js
+++ controllers/artistDetailCtrl.js
@@ -1,9 +1,20 @@
 define(['angular', 'lodash'], function (angular) {
   'use strict';
 
-  return ['$scope', '$log', '$window', function ($scope, $log, $window) {
+  return ['$scope', '$log', '$window', '$sce', function ($scope, $log, $window, $sce) {
     $log.info("ArtistDetailCtrl loaded");
-    
+
     $scope.artist = $window.__artist__;
+
+    $scope.artist.bio = $sce.trustAsHtml($scope.artist.bio);
+
+    $scope.albums = [[], [], []];
+
+    // Divide the albums up into 3 columns
+    var column = 0;
+    _.each($scope.artist.albums, function(album, i) {
+      $scope.albums[column].push(album);
+      column = (column == 2) ? 0 : column + 1;
+    });
   }];
 });
