38cd06ab6f35d1541c5bda2e2187287b4ea22a29
David Von Lehman <david.vonlehman@gmail.com>
2014-02-12 12:35:53 -0800

    Updates to unit test

--- test/homePageCtrl.js
+++ test/homePageCtrl.js
@@ -1,34 +1,45 @@
 // http://www.yearofmoo.com/2013/01/full-spectrum-testing-with-angularjs-and-karma.html#testing-controllers
-define(['angular', 'angularMocks', 'asset!controllers/homePageCtrl'], function (angular, mocks, homePageCtrl) {
+define(['angular', 'angularMocks', 'lodash', 'asset!controllers/homePageCtrl'], function (angular, mocks, _, homePageCtrl) {
 
 	describe("homePageCtrl", function() {
-		var scope, ctrl;
+		var scope, ctrl, windowMock;
 		beforeEach(function() {
-		  // mocks.module('homePageApp');
-		  var mockPageData = {
-		  	hotArtists: []
-		  };
-
-		  mocks.inject(function($rootScope, $controller) {
-	      scope = $rootScope.$new();
-	      ctrl = $controller(homePageCtrl, { $scope: scope, homePageData: mockPageData});
-		  });
+			windowMock = {
+				__config__: {
+					aerobaticAppOwner: 'test',
+					aerobaticAppRepo: 'test'
+				}
+			};
 		});
 
 		it('should have a properly working HomePageCtrl', function() {
-	    // var searchTestAtr = 'cars';
-	    // var response = $httpBackend.expectJSONP(
-	    //   'https://gdata.youtube.com/feeds/api/videos?q=' + searchTestAtr + '&v=2&alt=json&callback=JSON_CALLBACK');
-	    // response.respond(null);
+			windowMock.__topArtists__= [];
 
-	    // var scope = $rootScope.$new();
-	    // var ctrl = $controller('HomePageCtrl', {
-	    //   $scope : scope
-	    // });
+	    mocks.inject(function($rootScope, $controller) {
+	      scope = $rootScope.$new();
+	      ctrl = $controller(homePageCtrl, { $scope: scope, $window: windowMock});
+		  });
 
-	    assert.isDefined(scope.pageData);
-	    assert.isDefined(scope.pageData.hotArtists);
-	    assert.equal(scope.pageData.hotArtists.length, 0);
+	    assert.isDefined(scope.topArtists);
+	    assert.equal(scope.topArtists.length, 4);
 	  });
+
+	  it('should seperate hotArtists into 4 seperate columns', function() {
+	  	windowMock.__topArtists__ = _.map(_.range(10), function(i) {
+	  		return {
+	  			name: 'Artist ' + i
+	  		};
+	  	});
+
+			mocks.inject(function($rootScope, $controller) {
+	      scope = $rootScope.$new();
+	      ctrl = $controller(homePageCtrl, { $scope: scope, $window: windowMock });
+		  });
+
+		  assert.equal(2, scope.topArtists[0].length);
+		  assert.equal(3, scope.topArtists[1].length);
+		  assert.equal(3, scope.topArtists[2].length);
+		  assert.equal(2, scope.topArtists[3].length);
+	  })
 	});
 });
